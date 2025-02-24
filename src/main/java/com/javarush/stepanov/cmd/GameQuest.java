d6f78ac79d4f0e6c93f209cb14d271095e32f99a
user <stepanovruslan@mail.ru>
2025-02-25 01:51:42 +0300

    18 Quest service refactoring

--- src/main/java/com/javarush/stepanov/cmd/GameQuest.java
+++ src/main/java/com/javarush/stepanov/cmd/GameQuest.java
@@ -1,160 +1,86 @@
 package com.javarush.stepanov.cmd;
 
-import com.javarush.stepanov.entity.QuestInfoEntity;
 import com.javarush.stepanov.service.UserService;
 import com.javarush.stepanov.service.QuestService;
-import com.javarush.stepanov.util.UrlHelper;
 import jakarta.servlet.http.HttpServletRequest;
-
-import java.util.List;
-import java.util.Map;
-
-import static com.javarush.stepanov.constants.ConstantsCommon.*;
 
 @SuppressWarnings("unused")
 public class GameQuest implements Command {
     private final UserService userService;
-    private final List<QuestInfoEntity> questList;
-    private final Map<String, String> questMap;
-    private QuestInfoEntity conditionEntity;
-    private Integer time;
-    private Integer evidence;
-    private Integer gold;
-    private String pickedButton = LEFT;
-    private int step;
+    private final QuestService questService;
 
     public GameQuest(QuestService questService, UserService userService) {
         this.userService = userService;
-        this.questMap = questService.getQuestMap();
-        this.questList = questService.getQuestList();
+        this.questService = questService;
     }
 
     @Override
     public String doGet(HttpServletRequest req) {
-
         String paramName = req.getParameter("pickedButton");
         if (paramName != null) {
-            setCondition(req);
+            questService.setCondition(paramName);
         } else {
-            setStartCondition(req);
+            questService.setStartCondition();
+            String[] startViewInfo = questService.getStartViewInfo();
+            fillStartRequest(req,startViewInfo);
         }
 
-        if (step < questList.size()) {
-            fillRequest(req);
+        if  (questService.isNotEnding()){
+            String[] viewInfo = questService.getViewInfo();
+            fillRequest(req,viewInfo);
         }
-        step++;
+        questService.goNextStep();
 
-        if (lossCheck()) {
-            goToLoss(req);
-        } else if (winCheck()) {
-            goToWin(req);
+        if (questService.lossCheck()) {
+            String[] lossViewInfo = questService.getLossViewInfo();
+            goToLoss(req,lossViewInfo);
+        } else if (questService.winCheck()) {
+            String[] lossViewInfo = questService.getWinViewInfo();
+            goToWin(req,lossViewInfo);
         }
 
         return getView();
     }
 
-    private void setStartCondition(HttpServletRequest req) {
-        step = Integer.parseInt(questMap.get("START_STEP"));
-        time = Integer.parseInt(questMap.get("START_TIME"));
-        evidence = Integer.parseInt(questMap.get("START_EVIDENCE"));
-        gold = Integer.parseInt(questMap.get("START_GOLD"));
-        conditionEntity = questList.get(step);
-
-        String nameFileEvidence =  questMap.get("IMAGE_URL_EVIDENCE");
-        String urlEvidence = UrlHelper.createUrlFromFileName(nameFileEvidence);
-        req.getSession().setAttribute("IMAGE_URL_EVIDENCE",urlEvidence );
-
-        String nameFileGold =  questMap.get("IMAGE_URL_GOLD");
-        String urlGold = UrlHelper.createUrlFromFileName(nameFileGold);
-        req.getSession().setAttribute("IMAGE_URL_GOLD", urlGold);
-
-        String nameFileTime =  questMap.get("IMAGE_URL_TIME");
-        String urlTime = UrlHelper.createUrlFromFileName(nameFileTime);
-        req.getSession().setAttribute("IMAGE_URL_TIME", urlTime);
+    private void fillStartRequest(HttpServletRequest req, String[] startViews) {
+        req.getSession().setAttribute("IMAGE_URL_EVIDENCE",startViews[0] );
+        req.getSession().setAttribute("IMAGE_URL_GOLD", startViews[1]);
+        req.getSession().setAttribute("IMAGE_URL_TIME", startViews[2]);
     }
 
-    private void setCondition(HttpServletRequest req) {
-        if (step < questList.size()) {
-            conditionEntity = questList.get(step);
-        }
-        pickedButton = req.getParameter("pickedButton");
-        time += conditionEntity.getDeltaTime(pickedButton);
-        evidence += conditionEntity.getDeltaEvidence(pickedButton);
-        gold += conditionEntity.getDeltaGold(pickedButton);
+    private void fillRequest(HttpServletRequest req, String[] viewInfo) {
+        req.setAttribute("buttonLeft", viewInfo[0]);
+        req.setAttribute("buttonRight", viewInfo[1]);
+        req.setAttribute("result",  viewInfo[2]);
+        req.setAttribute("resultRight",  viewInfo[3]);
+        req.setAttribute("description",  viewInfo[4]);
+        req.setAttribute("time", viewInfo[5]);
+        req.setAttribute("evidence", viewInfo[6]);
+        req.setAttribute("gold",  viewInfo[7]);
+        req.setAttribute("imageUrl",  viewInfo[8]);
     }
 
-    private void fillRequest(HttpServletRequest req) {
-        req.setAttribute("buttonLeft", conditionEntity.getButtonLeftText());
-        req.setAttribute("buttonRight", conditionEntity.getButtonRightText());
-        req.setAttribute("result", (pickedButton.equals(LEFT))
-                ? conditionEntity.getResultLeftText()
-                : conditionEntity.getResultRightText());
-        req.setAttribute("resultRight", conditionEntity.getResultRightText());
-        req.setAttribute("description", conditionEntity.getDescription());
-        req.setAttribute("time", time);
-        req.setAttribute("evidence", evidence);
-        req.setAttribute("gold", gold);
-
-        String nameFileConditionEntity =   conditionEntity.getImageUrl();
-        String urlFileConditionEntity = UrlHelper.createUrlFromFileName(nameFileConditionEntity);
-        req.setAttribute("imageUrl", urlFileConditionEntity);
-    }
-
-    private boolean winCheck() {
-        return step == QUEST_END_STEP;
-    }
-
-    private boolean lossCheck() {
-        return (time <= QUEST_MIN_RESOURCE)
-                || (evidence <= QUEST_MIN_RESOURCE)
-                || (gold <= QUEST_MIN_RESOURCE)
-                || ((step == QUEST_LOSS_STEP) & (pickedButton.equals(RIGHT)));
-    }
-
-    private void goToWin(HttpServletRequest req) {
-        setWinAtributes(req, "DESCRIPTION_TEXT_WIN", "IMAGE_URL_WIN", "isWin");
+    private void goToWin(HttpServletRequest req, String[] winViewInfo) {
+        fillWinRequest(req, winViewInfo);
         addUserWin(req, userService);
     }
 
-    private void setWinAtributes(HttpServletRequest req, String DESCRIPTION_TEXT_WIN, String IMAGE_URL_WIN, String isWin) {
-        req.setAttribute("description", questMap.get(DESCRIPTION_TEXT_WIN));
-
-        String nameFileWin =  questMap.get(IMAGE_URL_WIN);
-        String urlFileWin = UrlHelper.createUrlFromFileName(nameFileWin);
-        req.setAttribute("imageUrl",urlFileWin);
-        req.setAttribute(isWin, true);
+    private void fillWinRequest(HttpServletRequest req, String [] winViewInfo) {
+        req.setAttribute("description", winViewInfo[0]);
+        req.setAttribute("imageUrl",winViewInfo[1]);
+        req.setAttribute("isWin", true);
     }
 
-    private void goToLoss(HttpServletRequest req) {
-        String lossСause = getLossCause();
-        setLossAtributes(req, lossСause);
+    private void goToLoss(HttpServletRequest req,String[] lossViewInfo) {
+        fillLossRequest(req, lossViewInfo);
         addUserLoss(req, userService);
-
     }
 
-    private void setLossAtributes(HttpServletRequest req, String lossСause) {
-        req.setAttribute("lossСause", lossСause);
-        req.setAttribute("description", questMap.get("DESCRIPTION_TEXT_LOSS"));
-        String nameFileLoss =  questMap.get("IMAGE_URL_LOSS");
-        String urlFileLoss = UrlHelper.createUrlFromFileName(nameFileLoss);
-        req.setAttribute("imageUrl", urlFileLoss);
+    private void fillLossRequest(HttpServletRequest req, String[] lossViewInfo) {
+        req.setAttribute("lossСause", lossViewInfo[0]);
+        req.setAttribute("description", lossViewInfo[1]);
+        req.setAttribute("imageUrl", lossViewInfo[2]);
         req.setAttribute("isLoss", true);
-    }
-
-    private String getLossCause() {
-        String lossСause;
-        if (time == QUEST_MIN_RESOURCE) {
-            lossСause = questMap.get("CAUSE_TEXT_TIME_LOSS");
-        } else if (gold == QUEST_MIN_RESOURCE) {
-            lossСause = questMap.get("CAUSE_TEXT_GOLD_LOSS");
-        } else if (evidence == QUEST_MIN_RESOURCE) {
-            lossСause = questMap.get("CAUSE_TEXT_EVIDENCE_LOSS");
-        } else if (step == QUEST_LOSS_STEP && pickedButton.equals(RIGHT)) {
-            lossСause = questMap.get("CAUSE_TEXT_WRONG_STEP_LOSS");
-        } else {
-            lossСause = questMap.get("CAUSE_TEXT_UNKNOWN_LOSS");
-        }
-        return lossСause;
     }
 
 }
--- src/main/java/com/javarush/stepanov/service/QuestService.java
+++ src/main/java/com/javarush/stepanov/service/QuestService.java
@@ -4,20 +4,34 @@ import com.javarush.stepanov.entity.QuestInfoEntity;
 import com.javarush.stepanov.entity.QuestMap;
 import com.javarush.stepanov.repository.QuestInfoEntityRepository;
 import com.javarush.stepanov.repository.QuestMapRepository;
+import com.javarush.stepanov.util.UrlHelper;
 
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
+import static com.javarush.stepanov.constants.ConstantsCommon.*;
+import static com.javarush.stepanov.constants.ConstantsCommon.RIGHT;
+
 public class QuestService {
 
     private final QuestMapRepository questMapRepository;
-
     private final QuestInfoEntityRepository questInfoEntityRepository;
+
+    private final List<QuestInfoEntity> questList;
+    private final Map<String, String> questMap;
+    private QuestInfoEntity conditionEntity;
+    private Integer time;
+    private Integer evidence;
+    private Integer gold;
+    private String pickedButton = LEFT;
+    private int step;
 
     public QuestService(QuestMapRepository questMapRepository, QuestInfoEntityRepository questInfoEntityRepository) {
         this.questMapRepository = questMapRepository;
         this.questInfoEntityRepository = questInfoEntityRepository;
+        questList = getQuestList();
+        questMap = getQuestMap();
     }
 
     public List<QuestInfoEntity> getQuestList() {
@@ -33,4 +47,106 @@ public class QuestService {
         return map;
     }
 
+    public String[] getWinViewInfo() {
+        String[] winViewInfo = new String[2];
+        winViewInfo[0] = questMap.get("DESCRIPTION_TEXT_WIN");
+        winViewInfo[1] =  getImgViewFromMap("IMAGE_URL_WIN");
+        return winViewInfo;
+    }
+
+    public String[] getLossViewInfo() {
+        String[] lossViewInfo = new String[3];
+        lossViewInfo[0] = getLossCause();
+        lossViewInfo[1] = questMap.get("DESCRIPTION_TEXT_LOSS");
+        lossViewInfo[2] =  getImgViewFromMap("IMAGE_URL_LOSS");
+        return lossViewInfo;
+    }
+
+    public String[] getViewInfo() {
+        String[] viewInfo = new String[9];
+        viewInfo[0] = conditionEntity.getButtonLeftText();
+        viewInfo[1] = conditionEntity.getButtonRightText();
+        viewInfo[2] = (pickedButton.equals(LEFT))
+                ? conditionEntity.getResultLeftText()
+                : conditionEntity.getResultRightText();
+        viewInfo[3] = conditionEntity.getResultRightText();
+        viewInfo[4] =  conditionEntity.getDescription();
+        viewInfo[5] =  time.toString();
+        viewInfo[6] =  evidence.toString();
+        viewInfo[7] =  gold.toString();
+        viewInfo[8] =  getImgViewFromCondition();
+        return viewInfo;
+    }
+
+    public boolean isNotEnding(){
+        return (step < questList.size());
+    }
+
+    public void setStartCondition() {
+        step = Integer.parseInt(questMap.get("START_STEP"));
+        time = Integer.parseInt(questMap.get("START_TIME"));
+        evidence = Integer.parseInt(questMap.get("START_EVIDENCE"));
+        gold = Integer.parseInt(questMap.get("START_GOLD"));
+        conditionEntity = questList.get(step);
+       
+    }
+    
+    public String[] getStartViewInfo(){
+        String[] startViews = new String[3];
+        startViews[0] = getImgViewFromMap("IMAGE_URL_EVIDENCE");
+        startViews[1] = getImgViewFromMap("IMAGE_URL_GOLD");
+        startViews[2] = getImgViewFromMap("IMAGE_URL_TIME");
+        return startViews;
+    }
+
+    private String getImgViewFromMap(String imgKey){
+        String nameFile =  questMap.get(imgKey);
+        return UrlHelper.createUrlFromFileName(nameFile);
+    }
+
+    private String getImgViewFromCondition(){
+        String nameFile =  conditionEntity.getImageUrl();
+        return UrlHelper.createUrlFromFileName(nameFile);
+    }
+
+    public void setCondition(String pickedButton) {
+        if (step < questList.size()) {
+            conditionEntity = questList.get(step);
+        }
+        time += conditionEntity.getDeltaTime(pickedButton);
+        evidence += conditionEntity.getDeltaEvidence(pickedButton);
+        gold += conditionEntity.getDeltaGold(pickedButton);
+        this.pickedButton = pickedButton;
+    }
+
+    public void goNextStep() {
+        step++;
+    }
+
+    public boolean winCheck() {
+        return step == QUEST_END_STEP;
+    }
+
+    public boolean lossCheck() {
+        return (time <= QUEST_MIN_RESOURCE)
+                || (evidence <= QUEST_MIN_RESOURCE)
+                || (gold <= QUEST_MIN_RESOURCE)
+                || ((step == QUEST_LOSS_STEP) & (pickedButton.equals(RIGHT)));
+    }
+
+    private String getLossCause() {
+        String lossСause;
+        if (time == QUEST_MIN_RESOURCE) {
+            lossСause = questMap.get("CAUSE_TEXT_TIME_LOSS");
+        } else if (gold == QUEST_MIN_RESOURCE) {
+            lossСause = questMap.get("CAUSE_TEXT_GOLD_LOSS");
+        } else if (evidence == QUEST_MIN_RESOURCE) {
+            lossСause = questMap.get("CAUSE_TEXT_EVIDENCE_LOSS");
+        } else if (step == QUEST_LOSS_STEP && pickedButton.equals(RIGHT)) {
+            lossСause = questMap.get("CAUSE_TEXT_WRONG_STEP_LOSS");
+        } else {
+            lossСause = questMap.get("CAUSE_TEXT_UNKNOWN_LOSS");
+        }
+        return lossСause;
+    }
 }
