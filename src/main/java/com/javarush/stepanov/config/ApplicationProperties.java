59f3e6a43498c6c36788d2db919847dba81ef8fa
user <stepanovruslan@mail.ru>
2025-03-07 00:52:54 +0300

    46 Application properties find environment added

--- src/main/java/com/javarush/stepanov/config/ApplicationProperties.java
+++ src/main/java/com/javarush/stepanov/config/ApplicationProperties.java
@@ -1,6 +1,7 @@
 package com.javarush.stepanov.config;
 
 import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 
 import java.io.FileReader;
 import java.net.URI;
@@ -9,6 +10,7 @@ import java.nio.file.Paths;
 import java.util.Objects;
 import java.util.Properties;
 
+@Slf4j
 public class ApplicationProperties extends Properties {
 
     public static final String HIBERNATE_CONNECTION_URL = "hibernate.connection.url";
@@ -19,6 +21,7 @@ public class ApplicationProperties extends Properties {
     @SneakyThrows
     public ApplicationProperties() {
         this.load(new FileReader(CLASSES_ROOT + "/application.properties"));
+        injectEnvironmentVariables();
         try {
             String driver = this.getProperty(HIBERNATE_CONNECTION_DRIVER_CLASS);
             Class.forName(driver);
@@ -27,10 +30,37 @@ public class ApplicationProperties extends Properties {
         }
     }
 
+    private void injectEnvironmentVariables() {
+        log.info("Environment size: {}", System.getenv().size());
+        System.getenv().forEach((k, v) -> log.info("{} = {}", k, v));
+        this.forEach((key, value) -> {
+            String strKey = key.toString();
+            String strValue = value.toString();
+            if (strValue.startsWith("${") && strValue.endsWith("}")) {
+                String[] valueParts = strValue
+                        .replace("${","")
+                        .replace("}","")
+                        .split(":", 2);
+                if (valueParts.length == 2) {
+                    strValue = valueParts[1];
+                }
+                String keyEnvironmentVariable = valueParts[0];
+                String valueEnvironmentVariable = System.getenv(keyEnvironmentVariable);
+                if (keyEnvironmentVariable != null && valueEnvironmentVariable != null) {
+                    strValue = valueEnvironmentVariable;
+                }
+                this.setProperty(strKey, strValue);
+            }
+        });
+    }
+
+    //any runtime
     public final static Path CLASSES_ROOT = Paths.get(URI.create(
             Objects.requireNonNull(
                     ApplicationProperties.class.getResource("/")
             ).toString()));
 
+    //only in Tomcat (not use in tests)
     public final static Path WEB_INF = CLASSES_ROOT.getParent();
 }
+
