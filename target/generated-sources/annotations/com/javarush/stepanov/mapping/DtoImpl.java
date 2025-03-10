d3d97de6f5d614798d84c9153bd0968e8f8dc99b
user <stepanovruslan@mail.ru>
2025-03-10 12:59:41 +0300

    53 compouse working with 1 docker files

--- target/generated-sources/annotations/com/javarush/stepanov/mapping/DtoImpl.java
+++ target/generated-sources/annotations/com/javarush/stepanov/mapping/DtoImpl.java
@@ -8,7 +8,7 @@ import javax.annotation.processing.Generated;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-03-10T12:36:25+0300",
+    date = "2025-03-10T12:54:31+0300",
     comments = "version: 1.5.5.Final, compiler: javac, environment: Java 21.0.4 (Oracle Corporation)"
 )
 public class DtoImpl implements Dto {
